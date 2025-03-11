d5b7943b33b488d3cef249cb653033c984eec30f
Gaureesh Hegde <gaureesh.hegde@gmail.com>
2025-03-11 23:01:40 +0530

    Made a small change to hello.cpp to trigger the webhook

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,24 +1,13 @@
-<<<<<<< HEAD:main/hello.cpp
 //File: hello.cpp
 #include <iostream>
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
-  return 0;
-}
-=======
-//File: hello.cpp
-#include <iostream>
-using namespace std;
+  cout << "Hello, World!\n";
 
-int main(void) {
-  cout << "Hello, World\n";
-  
   cout << "Hello, Jenkins\n";
+
   cout << "I have successfully built and run this  \n";
   return 0;
 }
->>>>>>> e4c0aad (Updated hello.cpp to test GitHub webhook):hello.cpp
+
