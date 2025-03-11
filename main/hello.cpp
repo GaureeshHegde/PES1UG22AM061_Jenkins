9fd33941bda9e4d792c5737794c72254d2833bad
Gaureesh Hegde <gaureesh.hegde@gmail.com>
2025-03-11 22:46:35 +0530

    Re-added main folder and moved files inside

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:main/hello.cpp
 //File: hello.cpp
 #include <iostream>
 using namespace std;
@@ -8,3 +9,16 @@ int main(void) {
   cout << "I have successfully built and run this  \n";
   return 0;
 }
+=======
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run this  \n";
+  return 0;
+}
+>>>>>>> e4c0aad (Updated hello.cpp to test GitHub webhook):hello.cpp
